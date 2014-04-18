d3b348dee21e5c1c03626fdb7861dceca38dbb75
Maik Schünemann <maikschuenemann@gmail.com>
2014-04-18 16:12:22 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to read-digits
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/read_digits/core_test.clj
@@ -0,0 +1,7 @@
+(ns read-digits.core-test
+  (:require [clojure.test :refer :all]
+            [read-digits.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
